bbf511f257f868775f6da59ea5510236a9a6f144
RenatoB_Alves <44118628+RenatoGoodEnd@users.noreply.github.com>
2023-01-24 20:42:06 -0300

    iniciando o arquivo de assincrono

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* Modo Assíncrono usando o método .then e .catch
+var  consultaCEP  =  fetch ( 'https://viacep.com.br/ws/01001000/json/' )
+    .then ( resposta  =>  resposta . json ( ) )  // o.JSON converte a resposta em informação acessível pelo programador
+    .then ( r  =>  {
+        if  ( r.erro )  {
+            throw  Error ( 'Esse cep não existe!' )
+        }  else
+            console.log ( r )
+    } )
+    .catch ( erro  =>  console.log ( erro ) )
+    .finaly ( mensagem  =>  console.log ( 'Processamento concluido!' ) ) ;
+
+console.log ( consultaCEP ) ;
+*/
+
 async function buscaEndereco(cep) {
     var mensagemErro = document.getElementById('erro');
     mensagemErro.innerHTML = "";
